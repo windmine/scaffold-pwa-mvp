e61c8bf481391b1993edf500580f184c47dbfa71
windmine <126423331+windmine@users.noreply.github.com>
2026-05-14 17:25:05 +1200

    local network test.
    build and test on local host and 192.168.x.x:8080

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    basicSsl()
+  ],
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    strictPort: true
+  }
 })
